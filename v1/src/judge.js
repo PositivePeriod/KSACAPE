ba5851dcfd3b7d018475f3b1baea7bd94f4657be
PositivePeriod <positiveperiod.dev@gmail.com>
2021-04-22 21:42:12 +0900

    fix: change in to includes

--- v1/src/judge.js
+++ v1/src/judge.js
@@ -1,9 +1,7 @@
 const PROBLEM = require('../data/problem.json');
 const PROBLEM_KEY = Object.keys(PROBLEM);
-console.log(PROBLEM_KEY);
 
 function isValidID(id) {
-    console.log(id, PROBLEM_KEY, id in PROBLEM_KEY);
     return PROBLEM_KEY.includes(id);
 }
 
