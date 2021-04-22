3fb67575ee738745b672de4c6caf5fc1ced5c25c
PositivePeriod <positiveperiod.dev@gmail.com>
2021-04-22 18:44:15 +0900

    feat: input validation

--- v1/src/judge.js
+++ v1/src/judge.js
@@ -1,13 +1,19 @@
 const PROBLEM = require('../data/problem.json');
+const PROBLEM_KEY = Object.keys(PROBLEM);
+console.log(PROBLEM_KEY);
 
-class Judge {
-    constructor() {
-        this.problem = Object.freeze(PROBLEM);
-    }
-
-    check(id, answer) {
-        return this.problem[id]['answer'] === answer? this.problem[id]['next']: null;
-    }
+function isValidID(id) {
+    console.log(id, PROBLEM_KEY, id in PROBLEM_KEY);
+    return PROBLEM_KEY.includes(id);
 }
 
-module.exports = new Judge();
+function isAnswer(id, answer) {
+    if (isValidID(id)) {
+        if (PROBLEM[id]['answer'] === answer) {
+            return PROBLEM[id]['next'];
+        }
+    }
+    return null;
+}
+
+module.exports = { isValidID: isValidID, isAnswer: isAnswer };
